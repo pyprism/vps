15e1986696672ead7bac1a560a095e665eb56d1d
Ashutosh Das <pyprism@users.noreply.github.com>
2013-10-26 01:57:18 +0600

    nginx , mongo , nodejs

--- hiren.sh
+++ hiren.sh
@@ -23,6 +23,7 @@ add-apt-repository ppa:nginx/stable -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
 
+#mongo , nginx and nodejs installation
 apt-get update
 apt-get install nodejs nginx mongodb-10gen -y
 echo "nodejs , nginx , mongodb installation completed "
