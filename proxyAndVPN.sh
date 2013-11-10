9ecc6e992936ff31ac3730a280100875210fa1a4
pyprism <areose21@gmail.com>
2013-11-10 14:55:19 +0600

    update proxy script

--- proxyAndVPN.sh
+++ proxyAndVPN.sh
@@ -11,3 +11,10 @@ apt-get update
 
 echo "Proxy Or VPN ?"
 echo "Press 1 For Proxy , Press 2 For VPN"
+read choose
+
+$ipaddrs = ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' 
+
+if [ $choose == 1 ];then
+	apt-get install python-m2crypto python-gevent python-pip -y
+	pip install shadowsocks
