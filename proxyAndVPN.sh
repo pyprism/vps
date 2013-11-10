98d683eea546a1741ce255de205c9d3bb31c0ca3
pyprism <areose21@gmail.com>
2013-11-10 14:44:42 +0600

    add proxy/vpn script

---
+++ proxyAndVPN.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Date: Nov 10 , 2013 #hiren  :D
+
+echo "~~~~~~~~~~~~~~~~~"
+echo "|               |"
+echo "| System Update |"
+echo "|               |"
+echo "~~~~~~~~~~~~~~~~~"
+apt-get update
+
+echo "Proxy Or VPN ?"
+echo "Press 1 For Proxy , Press 2 For VPN"
