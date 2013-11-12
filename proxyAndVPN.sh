8834f78a2c566ffe19bce7660a5d3339fdfbe764
Ashutosh Das <pyprism@users.noreply.github.com>
2013-11-12 22:31:29 +0600

    Update proxyAndVPN.sh

--- proxyAndVPN.sh
+++ proxyAndVPN.sh
@@ -18,4 +18,7 @@ $ipaddrs = ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 if [ $choose == '1' ]; then
 	apt-get install python-m2crypto python-gevent python-pip -y
 	pip install shadowsocks
+	mkdir proxy && cd proxy
+	echo "{" >> config.json
+	echo ""server":$ipaddrs," >> config.json
 fi
