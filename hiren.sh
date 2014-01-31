f479810855995decd5baccbcfd96cd2752304444
Ashutosh Das <pyprism@users.noreply.github.com>
2014-02-01 05:44:51 +0600

    Update hiren.sh

--- hiren.sh
+++ hiren.sh
@@ -75,4 +75,11 @@ echo "user created"
 echo "Enter password for new user:"
 passwd $username
 
+#MariaD
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943dbb
+sudo add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu saucy main'
+apt-get update
+apt-get install mariadb-server
+
+
 git clone https://github.com/pyprism/vps.git
