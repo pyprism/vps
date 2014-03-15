81f4d308b1d4b57d66e3a0022a953daaac2d98d6
Ashutosh Das <pyprism@users.noreply.github.com>
2014-03-15 18:00:16 +0600

    add comfrimation message hiren ;)

--- hiren.sh
+++ hiren.sh
@@ -8,14 +8,20 @@ if [ "$(id -u)" != "0" ]; then
 	echo "Sorry man, you are not Mr.Root !"
 	exit 1
 fi
+
+function nisha (){
+ echo '######################################################'
+ echo "##              $1                                  ##"
+ echo '######################################################'
+}
 #now system update 
 apt-get update
 apt-get dist-upgrade -y
-echo "Your system up2date"
+nisha "System Uptodate "
 
 #basic package installation
 apt-get install vnstat youtube-dl finger htop axel fail2ban sendmail git python-software-properties software-properties-common python-pip nethogs unzip nmap -y
-echo "Basic package installation complete"
+nisha "Basic package installation complete"
 
 #ppa add
 add-apt-repository ppa:chris-lea/node.js -y
@@ -26,7 +32,7 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 #mongo , nginx and nodejs installation
 apt-get update
 apt-get install nodejs nginx mongodb-10gen -y
-echo "nodejs , nginx , mongodb installation completed "
+nisha "nodejs , nginx , mongodb installation complete "
 
 #php
 apt-get install php-apc php5 php5-apcu php5-pgsql php5-fpm php5-xcache php5-memcached php5-json php5-memcache php5-mcrypt php5-imagick php5-geoip php5-gd php5-dev php5-curl php5-cli php5-mysql
@@ -34,11 +40,12 @@ apt-get install php-apc php5 php5-apcu php5-pgsql php5-fpm php5-xcache php5-memc
 #composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+nisha "composer installation complete"
 
 #proxy shadowsocks
 pip install shadowsocks
 apt-get install python-m2crypto python-gevent -y
-echo "proxy installed"
+nisha "python proxy installation complete"
 
 #512 swap ! 
 dd if=/dev/zero of=/swapfile bs=1024 count=512k
@@ -48,7 +55,7 @@ echo "/swapfile       none    swap    sw      0       0 " >> /etc/fstab
 echo 0 > /proc/sys/vm/swappiness
 chown root:root /swapfile
 chmod 0600 /swapfile
-echo "Swap enabled"
+nisha "Swap configuration complete"
 
 #fish shell
 apt-add-repository ppa:fish-shell/release-2 -y
@@ -58,7 +65,7 @@ chsh -s $a
 
 #extra entropy
 apt-get install haveged -y
-echo " Haveged installed"
+nisha " Haveged installed"
 
 #vagrant 
 add-apt-repository ppa:ikuya-fruitsbasket/virtualbox -y
@@ -69,6 +76,7 @@ dpkg -i vagrant_1.5.0_i686.deb
 apt-get install linux-headers-$(uname -r) -y
 dpkg-reconfigure virtualbox-dkms
 vagrant box add precise32 http://files.vagrantup.com/precise32.box
+nisha "vagrant installation complete"
 
 #create new user
 echo ":::::Create New User:::::"
@@ -84,6 +92,7 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082
 sudo add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu saucy main'
 apt-get update
 apt-get install mariadb-server -y
+nisha "MariaDb installation complete"
 
 #Mysql Secure
 mysql_secure_installation
@@ -91,13 +100,18 @@ mysql_secure_installation
 #Postgresql Latest version
 add-apt-repository ppa:chris-lea/postgresql-9.3 -y
 apt-get update && apt-get install -y postgresql-9.3
+nisha "PostgreSQL Complete"
 
 #redis ! ? :D
 add-apt-repository ppa:chris-lea/redis-server -y
 apt-get update
 apt-get install redis-server -y
+nisha "Redis Complete"
 
 #meteorjs
 curl https://install.meteor.com/ | sh
+nisha "Meteorjs Complete"
 
 git clone https://github.com/pyprism/vps.git
+
+nisha "All Done . Check If There Is Any Err. "
