35bfc99a7cef812326b5388d2fee2610bc629a75
Ashutosh Das <pyprism@users.noreply.github.com>
2014-05-12 23:59:01 +0600

    Removed Nginx ppa

--- hiren.sh
+++ hiren.sh
@@ -28,7 +28,6 @@ nisha "Basic package installation complete"
 
 #ppa add
 add-apt-repository ppa:chris-lea/node.js -y
-add-apt-repository ppa:nginx/stable -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
 
