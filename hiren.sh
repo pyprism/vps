a898ab1206b9ac12f672060e33bd8df6f5989b27
Ashutosh Das <pyprism@users.noreply.github.com>
2014-10-11 20:40:22 +0600

    Update docker ppa

--- hiren.sh
+++ hiren.sh
@@ -118,10 +118,9 @@ nisha "Redis Complete"
 
 #docker.io
 apt-get install apt-transport-https -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sh -c "echo deb https://get.docker.io/ubuntu docker main /etc/apt/sources.list.d/docker.list"
+add-apt-repository ppa:docker-maint/testing
 apt-get update
-apt-get install lxc-docker
+apt-get install docker.io
 
 #https://github.com/lebinh/ngxtop
 pip install ngxtop virtualenv
