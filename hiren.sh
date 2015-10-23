da4959030b01464572218823dc87725edb65ebf8
Ashutosh Das <pyprism@users.noreply.github.com>
2015-10-23 15:50:19 +0600

    Update hiren.sh

--- hiren.sh
+++ hiren.sh
@@ -27,7 +27,7 @@ apt-get install libffi-dev vnstat youtube-dl finger htop python3-dev inxi axel f
 nisha "Basic package installation complete"
 
 #ppa add
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
