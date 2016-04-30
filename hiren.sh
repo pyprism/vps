cd505ed12e7bf219bf3e3335ccd56ecf0b1fbf74
Ashutosh Das <pyprism@users.noreply.github.com>
2016-04-30 20:34:16 +0600

    Update hiren.sh

--- hiren.sh
+++ hiren.sh
@@ -115,7 +115,7 @@ nisha "Redis Complete"
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > "/etc/apt/sources.list.d/docker.list"
 apt update
-apt install docker-engine
+apt install docker-engine -y
 nisha "Installed Docker"
 
 # some useful packages 
