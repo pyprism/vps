f52cfc5acabae67261615b11f6fafd077392bf84
Ashutosh Das <areose21@gmail.com>
2020-12-27 22:12:58 +0600

    Update hiren.sh

--- hiren.sh
+++ hiren.sh
@@ -28,14 +28,14 @@ add-apt-repository ppa:nilarimogard/webupd8 -y
 apt update
 
 #basic package installation
-apt-get install zsh ntp libffi-dev ncdu vnstat python-m2crypto libpango1.0-0 youtube-dl letsencrypt finger htop python3-dev inxi axel fail2ban python-dev sendmail git python-pip nethogs unzip nmap imagemagick webp -y
+apt-get install zsh ntp libffi-dev ncdu vnstat libpango1.0-0 youtube-dl letsencrypt finger htop python3-dev inxi axel fail2ban sendmail git nethogs unzip nmap imagemagick webp -y
 nisha "Basic package installation complete"
 
 # puppeter
 apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 #ppa add
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 add-apt-repository ppa:nginx/stable
 
 #nginx and nodejs installation
