d7c709f9a8d6e75b9f2e74a0dd1ccbd3b282ec8e
Ashutosh Das <areose21@gmail.com>
2022-09-05 18:44:02 +0600

    updated docker

--- hiren.sh
+++ hiren.sh
@@ -120,10 +120,13 @@ apt install redis -y
 nisha "Redis Complete"
 
 #docker.io
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 nisha "Installed Docker"
 
 # for  nginx
