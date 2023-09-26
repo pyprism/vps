74fe7dea9badf93ff2296fb6fa48b69a6c11cd8a
Ashutosh Das <areose21@gmail.com>
2023-09-26 15:02:22 +0600

    Update hiren.sh: change nodejs

--- hiren.sh
+++ hiren.sh
@@ -34,11 +34,15 @@ nisha "Basic package installation complete"
 # puppeter
 apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
-#ppa add
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-add-apt-repository ppa:nginx/stable
+# nodejs pre install
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-#nginx and nodejs installation
+
+# nginx and nodejs install
+add-apt-repository ppa:nginx/stable
 apt  update
 apt  install nodejs nginx -y
 nisha "nodejs , nginx installation complete "
