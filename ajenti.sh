45a2f399cf1770cfb937b3dfe4b8a45b10727311
Ashutosh Das <git.pyprism@gmail.com>
2026-02-16 14:55:52 +0600

    nuked all unused files

--- ajenti.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget http://repo.ajenti.org/debian/key -O- | apt-key add -
-echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> /etc/apt/sources.list
-apt-get update && apt-get install ajenti
-service ajenti restart
-$ipaddrs = ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
-echo "Ajenti Control Panel Running On https://$ipaddrs:8000"
